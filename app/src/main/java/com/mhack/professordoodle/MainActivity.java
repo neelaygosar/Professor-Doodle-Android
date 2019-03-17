9d4d45073fddb83d4affea0d17c82b271df58fc4
10aditya <adityapingle90@gmail.com>
2019-03-17 12:04:34 +0530

    final

--- app/src/main/java/com/mhack/professordoodle/MainActivity.java
+++ app/src/main/java/com/mhack/professordoodle/MainActivity.java
@@ -1,6 +1,7 @@
 package com.mhack.professordoodle;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -11,55 +12,60 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
-import android.provider.Settings;
+import android.text.Layout;
 import android.util.Base64;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     DrawingView dv;
     private Paint mPaint;
-    private ImageView button;
+    private ImageView addImageButton;
+    private ImageView brushSizeButton;
     private int i = 0;
-    private String roomid;
+    private String roomId;
+    private ImageView colorButton;
+    private AlertDialog colorAlert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        roomId = getIntent().getStringExtra("roomid");
+
         dv = new DrawingView(this);
+
         setContentView(R.layout.activity_main);
-        RelativeLayout canvasLayout = findViewById(R.id.canvas);
+        RelativeLayout canvasLayout = findViewById(R.id.canvas_layout);
         canvasLayout.addView(dv, new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
-        setContentView(dv);
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
         mPaint.setDither(true);
@@ -68,27 +74,80 @@ public class MainActivity extends AppCompatActivity {
         mPaint.setStrokeJoin(Paint.Join.ROUND);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
         mPaint.setStrokeWidth(10);
-        button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
+        addImageButton = findViewById(R.id.add_image);
+        brushSizeButton = findViewById(R.id.brush_size);
+        colorButton = findViewById(R.id.color);
+        brushSizeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI), 1000);
+                openbrushsizedialog();
+            }
+        });
+        addImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dv.convertCanvasToImage();
+//                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI), 1000);
+            }
+        });
+        colorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opendialogue();
             }
         });
 
     }
+
+    private void openbrushsizedialog() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.seekbarlayout, null);
+        alert.setView(view);
+        final SeekBar seekBar = view.findViewById(R.id.seek);
+        seekBar.setProgress((int) mPaint.getStrokeWidth());
+        alert.create().show();
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mPaint.setStrokeWidth(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    public void opendialogue() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        View view = LayoutInflater.from(this).inflate(R.layout.color_dialog, null);
+        builder.setView(view);
+        view.findViewById(R.id.red).setOnClickListener(this);
+        view.findViewById(R.id.yellow).setOnClickListener(this);
+        view.findViewById(R.id.blue).setOnClickListener(this);
+        view.findViewById(R.id.green).setOnClickListener(this);
+        view.findViewById(R.id.white).setOnClickListener(this);
+        view.findViewById(R.id.black).setOnClickListener(this);
+        colorAlert = builder.create();
+        colorAlert.show();
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == 1000 && data.getData() != null) {
-            String uri= getRealPathFromURI(data.getData().toString());
-            //.setImageBitmap(BitmapFactory.decodeFile(uri));
-//            if(mCanvas!=null){
-                mCanvas = new Canvas(BitmapFactory.decodeFile(uri).copy(Bitmap.Config.ARGB_8888, true));
+            String uri = getRealPathFromURI(data.getData().toString());
+            mCanvas = new Canvas(BitmapFactory.decodeFile(uri).copy(Bitmap.Config.ARGB_8888, true));
+        }
+    }
 
-//            }
-
-
-        }}
     private String getRealPathFromURI(String contentURI) {
         Uri contentUri = Uri.parse(contentURI);
         Cursor cursor = getContentResolver().query(contentUri, null, null, null, null);
@@ -102,9 +161,42 @@ public class MainActivity extends AppCompatActivity {
             return t;
         }
     }
+
     private Canvas mCanvas;
 
-    public class DrawingView extends View {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.red:
+                mPaint.setColor(ContextCompat.getColor(MainActivity.this, R.color.red));
+                colorAlert.dismiss();
+                break;
+            case R.id.blue:
+                mPaint.setColor(ContextCompat.getColor(MainActivity.this, R.color.blue));
+                colorAlert.dismiss();
+                break;
+            case R.id.yellow:
+                mPaint.setColor(ContextCompat.getColor(MainActivity.this, R.color.yellow));
+                colorAlert.dismiss();
+                break;
+            case R.id.green:
+                mPaint.setColor(ContextCompat.getColor(MainActivity.this, R.color.green));
+                colorAlert.dismiss();
+                break;
+            case R.id.white:
+                mPaint.setColor(ContextCompat.getColor(MainActivity.this, R.color.white));
+                colorAlert.dismiss();
+                break;
+            case R.id.black:
+                mPaint.setColor(ContextCompat.getColor(MainActivity.this, R.color.black));
+                colorAlert.dismiss();
+                break;
+
+        }
+
+    }
+
+    public class DrawingView extends View implements RequestCallback {
 
         public int width;
         public int height;
@@ -129,16 +221,21 @@ public class MainActivity extends AppCompatActivity {
             circlePaint.setStyle(Paint.Style.STROKE);
             circlePaint.setStrokeJoin(Paint.Join.MITER);
             circlePaint.setStrokeWidth(4f);
+
+            getUpdatedImage(DrawingView.this);
         }
+
+        int mw, mh;
 
         @Override
         protected void onSizeChanged(int w, int h, int oldw, int oldh) {
             super.onSizeChanged(w, h, oldw, oldh);
-
             mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
             mCanvas = new Canvas(mBitmap);
             mCanvas.drawARGB(255, 255, 255, 255);
             uploadCanvas();
+            mw = w;
+            mh = h;
         }
 
         @Override
@@ -169,7 +266,6 @@ public class MainActivity extends AppCompatActivity {
                 circlePath.reset();
                 circlePath.addCircle(mX, mY, 30, Path.Direction.CW);
             }
-
         }
 
         private void touch_up() {
@@ -219,12 +315,11 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                    mBitmap.compress(Bitmap.CompressFormat.PNG,80, byteArrayOutputStream);
+                    mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
                     byte[] bytes = byteArrayOutputStream.toByteArray();
-                    String base64 = "data:image/png;base64," + Base64.encodeToString(bytes, Base64.DEFAULT);
-                    base64 = base64.replace("\n", "|");
+                    String base64 = "data:image/png;base64," + Base64.encodeToString(bytes, Base64.DEFAULT).replace("\n", "|");
                     try {
-                        URL url = new URL("http://192.168.29.40:2234/drawing.php");
+                        URL url = new URL("http://192.168.43.40:2234/drawing.php");
                         httpURLConnection = (HttpURLConnection) url.openConnection();
 //                        httpURLConnection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
                         httpURLConnection.setReadTimeout(10000);
@@ -240,14 +335,14 @@ public class MainActivity extends AppCompatActivity {
 */
                         JSONObject jsonObject = new JSONObject();
                         jsonObject.put("image", base64);
-                        jsonObject.put("timestamp", String.valueOf(System.currentTimeMillis() / 1000));
+//                        jsonObject.put("timestamp", String.valueOf(System.currentTimeMillis() / 1000));
 
-                        if (first) {
-                            jsonObject.put("first", "1");
-                        } else {
-                            jsonObject.put("first", "0");
-                            jsonObject.put("id", objectid);
-                        }
+//                        if (first) {
+//                            jsonObject.put("first", "1");
+//                        } else {
+//                            jsonObject.put("first", "0");
+                        jsonObject.put("id", roomId);
+//                        }
 //                        String query = uriBuilder.build().getEncodedQuery();
 
                         System.out.println(jsonObject.toString());
@@ -270,11 +365,11 @@ public class MainActivity extends AppCompatActivity {
                         while ((line = reader.readLine()) != null) {
                             builder.append(line);
                         }
-                        if (first) {
+                  /*      if (first) {
                             objectid = builder.toString();
                             System.out.println(objectid);
                         }
-                        first = false;
+                        first = false;*/
                     } catch (MalformedURLException e) {
                         e.printStackTrace();
                     } catch (IOException e) {
@@ -291,52 +386,75 @@ public class MainActivity extends AppCompatActivity {
             }).start();
         }
 
-        private void convertCanvasToImage() {
+        void convertCanvasToImage() {
             FileOutputStream fileOutputStream = null;
+           String filename = Environment.getExternalStorageDirectory().getAbsolutePath() + "/doodle" + System.currentTimeMillis() / 1000 + ".jpeg";
             try {
-                fileOutputStream = new FileOutputStream(getExternalCacheDir() + "/temp.jpeg");
+                fileOutputStream = new FileOutputStream(filename);
             } catch (Exception e) {
             }
             mBitmap.compress(Bitmap.CompressFormat.JPEG, 80, fileOutputStream);
+            Toast.makeText(MainActivity.this,"Image downloaded as "+ filename, Toast.LENGTH_LONG).show();
         }
 
+        @Override
+        public void onSuccess(String data) {
+            data = data.substring(22);
+            Log.i("data received ", data);
+            byte[] decodedString = Base64.decode(data, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+//            mCanvas = new Canvas(Bitmap.createBitmap(mw,mh, Bitmap.Config.ARGB_8888));
+//            Bitmap bitmap1 = Bitmap.createBitmap(bitmap, 0, 0, mw, mh);
+            mCanvas.drawBitmap(Bitmap.createScaledBitmap(bitmap, mw, mh, false), 0, 0, null);
+            invalidate();
+        }
     }
 
+    private void getUpdatedImage(final RequestCallback requestCallback) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection httpURLConnection = null;
+                while (true) {
+                    try {
+                        URL url = new URL("http://192.168.43.40:2234/drawingforandroid.php?id=" + roomId);
+                        httpURLConnection = (HttpURLConnection) url.openConnection();
+                        httpURLConnection.setRequestMethod("GET");
+                        httpURLConnection.setReadTimeout(10000);
+                        httpURLConnection.setConnectTimeout(15000);
 
-    private void createRoom() throws IOException {
-        HttpURLConnection httpURLConnection = null;
-        try {
-            URL url = new URL("http://192.168.29.40:2234/drawing.php");
-            httpURLConnection = (HttpURLConnection) url.openConnection();
-//                        httpURLConnection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-            httpURLConnection.setReadTimeout(10000);
-            httpURLConnection.setConnectTimeout(15000);
-            httpURLConnection.setRequestMethod("POST");
-            httpURLConnection.connect();
+                        httpURLConnection.connect();
 
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-            InputStream inputStream = httpURLConnection.getInputStream();
+                        InputStream inputStream = httpURLConnection.getInputStream();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            StringBuilder builder = new StringBuilder();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                        StringBuilder builder = new StringBuilder();
 
-            String line;
+                        String line;
 
-            while ((line = reader.readLine()) != null) {
-                builder.append(line);
+                        while ((line = reader.readLine()) != null) {
+                            builder.append(line);
+                        }
+
+                        requestCallback.onSuccess(builder.toString()
+                                .replace("|", "\n")
+                                .replace("\n\n", ""));
+
+                    } catch (Exception ignored) {
+
+                    } finally {
+                        if (httpURLConnection != null) {
+                            httpURLConnection.disconnect();
+                        }
+                    }
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-//            if (first) {
-                roomid = builder.toString();
-                System.out.println(roomid);
-//            }
-
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-            alertDialog.setTitle("Room Created");
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
+        }).start();
     }
 
 
